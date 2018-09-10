29a9a0c5cf17df4616e4830897f6feaacbdb52b7
Aaron Klump <sourcecode@intheloftstudios.com>
2018-09-09 19:11:46 -0700

    Refactor code small.

--- cloudy-app.sh
+++ cloudy-app.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 CLOUDY_SCRIPT="${BASH_SOURCE[0]}"
 while [ -h "$CLOUDY_SCRIPT" ]; do # resolve $CLOUDY_SCRIPT until the file is no longer a symlink
-  dir="$( cd -P "$( dirname "$CLOUDY_SCRIPT" )" && pwd )"
+  dir="$(cd -P "$(dirname "$CLOUDY_SCRIPT")" && pwd)"
   CLOUDY_SCRIPT="$(readlink "$CLOUDY_SCRIPT")"
   [[ $CLOUDY_SCRIPT != /* ]] && CLOUDY_SCRIPT="$dir/$CLOUDY_SCRIPT" # if $CLOUDY_SCRIPT was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
-ROOT="$( cd -P "$( dirname "$CLOUDY_SCRIPT" )" && pwd )"
-CLOUDY_CONFIG=$ROOT/script.example.config.yml
-source "$ROOT/cloudy/cloudy.sh"
+ROOT="$(cd -P "$( dirname "$CLOUDY_SCRIPT" )" && pwd)"
+CLOUDY_CONFIG=$ROOT/cloudy-app.yml
+source "$ROOT/install/cloudy/cloudy.sh"
 # End Cloudy Bootstrap
 
 # Input validation
--- install/script.example.sh
+++ install/script.example.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 CLOUDY_SCRIPT="${BASH_SOURCE[0]}"
 while [ -h "$CLOUDY_SCRIPT" ]; do # resolve $CLOUDY_SCRIPT until the file is no longer a symlink
-  dir="$( cd -P "$( dirname "$CLOUDY_SCRIPT" )" && pwd )"
+  dir="$(cd -P "$(dirname "$CLOUDY_SCRIPT")" && pwd)"
   CLOUDY_SCRIPT="$(readlink "$CLOUDY_SCRIPT")"
   [[ $CLOUDY_SCRIPT != /* ]] && CLOUDY_SCRIPT="$dir/$CLOUDY_SCRIPT" # if $CLOUDY_SCRIPT was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
-ROOT="$( cd -P "$( dirname "$CLOUDY_SCRIPT" )" && pwd )"
+ROOT="$(cd -P "$( dirname "$CLOUDY_SCRIPT" )" && pwd)"
 CLOUDY_CONFIG=$ROOT/script.example.config.yml
 source "$ROOT/cloudy/cloudy.sh"
 # End Cloudy Bootstrap
