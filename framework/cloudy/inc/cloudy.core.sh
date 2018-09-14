ad63e54b81ebf538bcfbbc4f738905fcb0320bcd
Aaron Klump <sourcecode@intheloftstudios.com>
2018-09-14 12:35:33 -0700

    Improve UI.

--- framework/cloudy/inc/cloudy.core.sh
+++ framework/cloudy/inc/cloudy.core.sh
@@ -433,7 +433,7 @@ function _cloudy_validate_command() {
     eval $(get_config_keys "commands")
     array_has_value__array=(${commands[@]})
     array_has_value "$command" && return 0
-    fail_because "You have called your script using a command, \"$command\", which does not exist."
+    fail_because "You have called $(basename $SCRIPT) using a command, \"$command\", which does not exist."
     return 1
 }
 
