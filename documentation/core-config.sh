8abab7e530c46065b1f28f53bb9cc555070bbb9e
Aaron Klump <sourcecode@intheloftstudios.com>
2018-10-05 16:32:16 -0700

    fix docs version

--- documentation/core-config.sh
+++ documentation/core-config.sh
@@ -62,7 +62,7 @@ pre_hooks = "api.php"
 #
 # The path to a .info file or a .json file containing 'version' as a first level key, whose value indicates the documentation version.
 # This can be relative to the directory containing core-config.sh or absolute if it begins with a /
-version_file = "../*.yml"
+version_file = "../cloudy_tools.yml"
 
 #
 # These paths are relative to the directory containing core-config.sh.
