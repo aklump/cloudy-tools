904c113f1ddae8fb0ada3b6bfc48a7b13a9298aa
Aaron Klump <sourcecode@intheloftstudios.com>
2019-05-05 18:48:26 -0700

    Update composer dependencies and optimize autoloader.

--- .web_package/hooks/build/00_composer_update.sh
+++ .web_package/hooks/build/00_composer_update.sh
@@ -4,7 +4,7 @@
 #
 
 composer=$(type composer >/dev/null 2>&1 && which composer)
-cd "$7/framework/cloudy/php" && $composer update
+cd "$7/framework/cloudy/php" && $composer update --optimize-autoloader
 
 git add "$7/framework/cloudy/php/vendor"
 git add "$7/framework/cloudy/php/composer.lock"
