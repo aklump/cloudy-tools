1696c2384278d12c4b70a284641a04dbf9d8d875
Aaron Klump <sourcecode@intheloftstudios.com>
2021-02-09 13:05:25 -0800

    Code formatting.

--- cloudy_tools.sh
+++ cloudy_tools.sh
@@ -6,43 +6,51 @@
 #
 
 # Define the configuration file relative to this script.
-CONFIG="cloudy_tools.yml";
+CONFIG="cloudy_tools.yml"
 
 # Uncomment this line to enable file logging.
 #LOGFILE="cloudy_tools.log"
 
 function on_boot() {
-    # Run the test command before the bootstrap to avoid conflicts.
-    [[ "$(get_command)" == "tests" ]] || return 0
-    source "$CLOUDY_ROOT/inc/cloudy.testing.sh"
-    do_tests_in "tests/cloudy.tests.sh"
-    exit_with_test_results
+  # Run the test command before the bootstrap to avoid conflicts.
+  [[ "$(get_command)" == "tests" ]] || return 0
+  source "$CLOUDY_ROOT/inc/cloudy.testing.sh"
+  do_tests_in "tests/cloudy.tests.sh"
+  exit_with_test_results
 }
 
 function on_compile_config() {
-    echo "$ROOT/cloudy_tools.runtime.yml"
+  echo "$ROOT/cloudy_tools.runtime.yml"
 }
 
 function rsync_framework() {
-    [[ "$framework" ]] || return 1
-    rsync -a $framework/cloudy/ ./cloudy  --exclude=*.log --exclude=cache/
+  [[ "$framework" ]] || return 1
+  rsync -a $framework/cloudy/ ./cloudy --exclude=*.log --exclude=cache/
 }
 
 function write_version_file() {
-    echo "cloudy_update__last_update=$(timestamp)" > $WDIR/cloudy/version.sh
+  echo "cloudy_update__last_update=$(timestamp)" >$WDIR/cloudy/version.sh
 
-    echo "cloudy_update__version=\"$(get_version)\"" >> $WDIR/cloudy/version.sh
-    echo "cloudy_update__source=\"https://github.com/aklump/cloudy\"" >> $WDIR/cloudy/version.sh
-    [ $? -eq 0 ] || fail_because "An error occurred writing $WDIR/cloudy/version.sh"
-    return $?
+  echo "cloudy_update__version=\"$(get_version)\"" >>$WDIR/cloudy/version.sh
+  echo "cloudy_update__source=\"https://github.com/aklump/cloudy\"" >>$WDIR/cloudy/version.sh
+  [ $? -eq 0 ] || fail_because "An error occurred writing $WDIR/cloudy/version.sh"
+  return $?
 }
 
 function validate_cloudy_instance_or_exit_with_failure() {
-    [ -d "$WDIR/cloudy" ] || exit_with_failure "No Cloudy framework found in this directory."
+  [ -d "$WDIR/cloudy" ] || exit_with_failure "No Cloudy framework found in this directory."
 }
 
 # Begin Cloudy Bootstrap
-s="${BASH_SOURCE[0]}";while [ -h "$s" ];do dir="$(cd -P "$(dirname "$s")" && pwd)";s="$(readlink "$s")";[[ $s != /* ]] && s="$dir/$s";done;r="$(cd -P "$(dirname "$s")" && pwd)";source "$r/framework/cloudy/cloudy.sh";[[ "$ROOT" != "$r" ]] && echo "$(tput setaf 7)$(tput setab 1)Bootstrap failure, cannot load cloudy.sh$(tput sgr0)" && exit 1
+s="${BASH_SOURCE[0]}"
+while [ -h "$s" ]; do
+  dir="$(cd -P "$(dirname "$s")" && pwd)"
+  s="$(readlink "$s")"
+  [[ $s != /* ]] && s="$dir/$s"
+done
+r="$(cd -P "$(dirname "$s")" && pwd)"
+source "$r/framework/cloudy/cloudy.sh"
+[[ "$ROOT" != "$r" ]] && echo "$(tput setaf 7)$(tput setab 1)Bootstrap failure, cannot load cloudy.sh$(tput sgr0)" && exit 1
 # End Cloudy Bootstrap
 
 # Input validation
@@ -57,155 +65,155 @@ installation_info_filepath="$WDIR/cloudy/version.sh"
 command=$(get_command)
 case $command in
 
-    "pm-clear-cache")
-        cache_dir="${CLOUDY_ROOT}/cache/cpm"
-        [[ ! "${CLOUDY_ROOT}" ]] && exit_with_failure "Invalid cache directory ${cache_dir}"
-        event_dispatch "pm_clear_cache" "$cache_dir" || exit_with_failure "Clearing pm caches failed."
-        if dir_has_files "$cache_dir"; then
-            clear=$(rm -rv "$cache_dir/"*)
-            status=$?
-            [ $status -eq 0 ] || exit_with_failure "Could not remove all cached pm files in $cache_dir"
-            file_list=($clear)
-            for i in "${file_list[@]}"; do
-               succeed_because "$(echo_green "$(basename $i)")"
-            done
-            exit_with_success "Local Cloudy package info has been flushed."
-        fi
-        exit_with_success "Cloudy package info will be fetched from the remote registry when needed."
-        ;;
+"pm-clear-cache")
+  cache_dir="${CLOUDY_ROOT}/cache/cpm"
+  [[ ! "${CLOUDY_ROOT}" ]] && exit_with_failure "Invalid cache directory ${cache_dir}"
+  event_dispatch "pm_clear_cache" "$cache_dir" || exit_with_failure "Clearing pm caches failed."
+  if dir_has_files "$cache_dir"; then
+    clear=$(rm -rv "$cache_dir/"*)
+    status=$?
+    [ $status -eq 0 ] || exit_with_failure "Could not remove all cached pm files in $cache_dir"
+    file_list=($clear)
+    for i in "${file_list[@]}"; do
+      succeed_because "$(echo_green "$(basename $i)")"
+    done
+    exit_with_success "Local Cloudy package info has been flushed."
+  fi
+  exit_with_success "Cloudy package info will be fetched from the remote registry when needed."
+  ;;
 
-    "pm-update")
-        source "$ROOT/inc/cloudy.pm.sh"
-        echo_title "Package Updater"
-        package=$(get_command_arg 0)
-        _cloudypm_update_package "$package" || fail
-        has_failed && [[ "$package" ]] && exit_with_failure "Could not update \"$package\"."
-        has_failed && exit_with_failure "Nothing updated."
-        exit_with_success_elapsed "Update process complete."
-        ;;
+"pm-update")
+  source "$ROOT/inc/cloudy.pm.sh"
+  echo_title "Package Updater"
+  package=$(get_command_arg 0)
+  _cloudypm_update_package "$package" || fail
+  has_failed && [[ "$package" ]] && exit_with_failure "Could not update \"$package\"."
+  has_failed && exit_with_failure "Nothing updated."
+  exit_with_success_elapsed "Update process complete."
+  ;;
 
-    "pm-install")
-        source "$ROOT/inc/cloudy.pm.sh"
-        echo_title "Package Installer"
-        package=$(get_command_arg 0)
-        ! has_option 'yes' && ! confirm --caution "Install $package in $WDIR?" && exit_with_failure "User cancelled."
-        _cloudypm_install_package $package
-        has_failed && exit_with_failure "Could not install \"$package\"."
-        exit_with_success_elapsed "$package was installed."
-        ;;
+"pm-install")
+  source "$ROOT/inc/cloudy.pm.sh"
+  echo_title "Package Installer"
+  package=$(get_command_arg 0)
+  ! has_option 'yes' && ! confirm --caution "Install $package in $WDIR?" && exit_with_failure "User cancelled."
+  _cloudypm_install_package $package
+  has_failed && exit_with_failure "Could not install \"$package\"."
+  exit_with_success_elapsed "$package was installed."
+  ;;
 
-    "flush")
-        validate_cloudy_instance_or_exit_with_failure
-        exit_with_cache_clear "$WDIR/cloudy"
-        ;;
+"flush")
+  validate_cloudy_instance_or_exit_with_failure
+  exit_with_cache_clear "$WDIR/cloudy"
+  ;;
 
-    "update")
-        echo_title "Cloudy Framework Updater"
+"update")
+  echo_title "Cloudy Framework Updater"
 
-        available_version=$(get_version)
+  available_version=$(get_version)
 
-        # Check for cloudy folder.
-        validate_cloudy_instance_or_exit_with_failure
-        [ -f "$installation_info_filepath" ] || exit_with_failure "Cannot determine installed version; missing file $installation_info_filepath."
-        source $installation_info_filepath
+  # Check for cloudy folder.
+  validate_cloudy_instance_or_exit_with_failure
+  [ -f "$installation_info_filepath" ] || exit_with_failure "Cannot determine installed version; missing file $installation_info_filepath."
+  source $installation_info_filepath
 
-        # Check current version of instance.
-        echo_key_value "Cloudy Directory" "$WDIR/cloudy"
-        echo_key_value "Installed Version" "$cloudy_update__version"
-        echo_key_value "Available System Version" "$available_version"
+  # Check current version of instance.
+  echo_key_value "Cloudy Directory" "$WDIR/cloudy"
+  echo_key_value "Installed Version" "$cloudy_update__version"
+  echo_key_value "Available System Version" "$available_version"
 
-        # Check installed version.
-        ! has_option "f" && [[ "$cloudy_update__version" == "$available_version" ]] && exit_with_success "You're already up-to-date."
+  # Check installed version.
+  ! has_option "f" && [[ "$cloudy_update__version" == "$available_version" ]] && exit_with_success "You're already up-to-date."
 
-        echo
-        [[ "$cloudy_update__version" != "$available_version" ]] && echo_yellow "Your version $cloudy_update__version is out-of-date." && echo
+  echo
+  [[ "$cloudy_update__version" != "$available_version" ]] && echo_yellow "Your version $cloudy_update__version is out-of-date." && echo
 
-        has_option "dry_run" && exit_with_success "This was a dry run; nothing was changed."
+  has_option "dry_run" && exit_with_success "This was a dry run; nothing was changed."
 
-        # Ask if they want to update.
-        ! has_option 'y' && ! confirm --caution "Do you want to update now?" && exit_with_failure "Update cancelled"
+  # Ask if they want to update.
+  ! has_option 'y' && ! confirm --caution "Do you want to update now?" && exit_with_failure "Update cancelled"
 
-        rsync_framework || exit_with_failure "An error occurred while updating this Cloudy framework."
-        write_version_file
+  rsync_framework || exit_with_failure "An error occurred while updating this Cloudy framework."
+  write_version_file
 
-        ! has_failed && exit_with_success "Framework has been updated"
-    ;;
+  ! has_failed && exit_with_success "Framework has been updated"
+  ;;
 
-    "core")
-        [ -e ./cloudy ] && exit_with_success "Cloudy is already installed.  Did you mean \"update\"?"
-        rsync_framework || fail_because "Could not copy Cloudy core to $WDIR."
-        if ! has_failed; then
-            write_version_file
-        fi
-        has_failed && exit_with_failure "Failed to install core in current directory."
-        write_log_notice "Installed Cloudy core in $WDIR"
-        exit_with_success_elapsed "Core installed."
-    ;;
+"core")
+  [ -e ./cloudy ] && exit_with_success "Cloudy is already installed.  Did you mean \"update\"?"
+  rsync_framework || fail_because "Could not copy Cloudy core to $WDIR."
+  if ! has_failed; then
+    write_version_file
+  fi
+  has_failed && exit_with_failure "Failed to install core in current directory."
+  write_log_notice "Installed Cloudy core in $WDIR"
+  exit_with_success_elapsed "Core installed."
+  ;;
 
-    "new")
-        basename=$(get_command_arg 0 "cloudy_script.sh")
-        script_filename=$(path_filename "$basename")
-        has_option 'config' && script_filename=$(path_filename $(get_option "config"))
-        config_file="$script_filename.yml"
-        has_option "json" && config_file="$script_filename.json"
+"new")
+  basename=$(get_command_arg 0 "cloudy_script.sh")
+  script_filename=$(path_filename "$basename")
+  has_option 'config' && script_filename=$(path_filename $(get_option "config"))
+  config_file="$script_filename.yml"
+  has_option "json" && config_file="$script_filename.json"
 
-        example_script="script.example.sh"
+  example_script="script.example.sh"
 
-        # Protect an existing script by that name.
-        [ -e "$basename" ] && ! has_option "force" && fail_because "$basename already exists. Use --force, -f to proceed."
+  # Protect an existing script by that name.
+  [ -e "$basename" ] && ! has_option "force" && fail_because "$basename already exists. Use --force, -f to proceed."
 
-        ! has_option 'y' && ! confirm --caution "Create $script_filename in the current directory?" && exit_with_failure "Nothing accomplished."
+  ! has_option 'y' && ! confirm --caution "Create $script_filename in the current directory?" && exit_with_failure "Nothing accomplished."
 
-        if ! has_failed; then
-            rsync_framework || fail_because "Could not copy Cloudy core to $WDIR."
-            cp $framework/script.sh ./
-            cp $framework/script.yml ./
+  if ! has_failed; then
+    rsync_framework || fail_because "Could not copy Cloudy core to $WDIR."
+    cp $framework/script.sh ./
+    cp $framework/script.yml ./
 
-            # The stub file script.sh
-            mv script.sh $basename || fail_because "Could not rename script.sh to $basename."
-            sed -i '' "s/__FILENAME/$script_filename/g" $basename || fail_because "Could not replace __FILENAME in $basename"
-            sed -i '' "s/__CONFIG/${config_file}/g" $basename || fail_because "Could not update config filepath in $basename."
+    # The stub file script.sh
+    mv script.sh $basename || fail_because "Could not rename script.sh to $basename."
+    sed -i '' "s/__FILENAME/$script_filename/g" $basename || fail_because "Could not replace __FILENAME in $basename"
+    sed -i '' "s/__CONFIG/${config_file}/g" $basename || fail_because "Could not update config filepath in $basename."
 
-            # Copy over examples.
-            if has_option "examples"; then
-                cp "$framework/script.example.sh" . || fail_because "Could not copy script.example.sh"
-                cp "$framework/script.example.yml" . || fail_because "Could not copy script.example.yml"
+    # Copy over examples.
+    if has_option "examples"; then
+      cp "$framework/script.example.sh" . || fail_because "Could not copy script.example.sh"
+      cp "$framework/script.example.yml" . || fail_because "Could not copy script.example.yml"
 
-                if has_option "json"; then
-                    sed -i '' "s/script.example.yml/script.example.json/g" script.example.sh || fail_because "Could not update config filepath in script.example.sh."
-                fi
-            fi
+      if has_option "json"; then
+        sed -i '' "s/script.example.yml/script.example.json/g" script.example.sh || fail_because "Could not update config filepath in script.example.sh."
+      fi
+    fi
 
-            # Convert YAML config files to JSON, if necessary.
-            if has_option "json"; then
-                echo $(php $CLOUDY_ROOT/php/config_to_json.php "$CLOUDY_ROOT/cloudy_config.schema.json" "script.yml") > ${config_file} || fail_because "Could not convert $(path_filename $config_file) to JSON"
-                [ $? -eq 0 ] && rm script.yml
+    # Convert YAML config files to JSON, if necessary.
+    if has_option "json"; then
+      echo $(php $CLOUDY_ROOT/php/config_to_json.php "$CLOUDY_ROOT/cloudy_config.schema.json" "script.yml") >${config_file} || fail_because "Could not convert $(path_filename $config_file) to JSON"
+      [ $? -eq 0 ] && rm script.yml
 
-                echo $(php $CLOUDY_ROOT/php/config_to_json.php "$CLOUDY_ROOT/cloudy_config.schema.json" "$WDIR/script.example.yml") > script.example.config.json || fail_because "Could not convert script.example.yml to JSON"
-                [ $? -eq 0 ] && rm script.example.yml
-            else
-                mv script.yml $config_file || fail_because "Could not rename script.yml to $config_file."
-            fi
+      echo $(php $CLOUDY_ROOT/php/config_to_json.php "$CLOUDY_ROOT/cloudy_config.schema.json" "$WDIR/script.example.yml") >script.example.config.json || fail_because "Could not convert script.example.yml to JSON"
+      [ $? -eq 0 ] && rm script.example.yml
+    else
+      mv script.yml $config_file || fail_because "Could not rename script.yml to $config_file."
+    fi
 
-            # Create a version stamp
-            if ! has_failed; then
-                write_version_file
-            fi
+    # Create a version stamp
+    if ! has_failed; then
+      write_version_file
+    fi
 
-            # Backout our files on failure.
-            if has_failed; then
-                [ -e "script.example.sh" ] && rm "script.example.sh"
-                [ -e "script.example.yml" ] && rm "script.example.yml"
-                [ -e "script.example.json" ] && rm "script.example.json"
-                [ -e $basename ] && rm $basename
-                [ -e $config_file ] && rm $config_file
-                [ -e cloudy ] && rm -r cloudy
-            fi
-        fi
-        has_failed && exit_with_failure "Failed to install $basename"
-        write_log_notice "Installed new script at $WDIR/$basename"
-        exit_with_success_elapsed "New script $basename created."
-    ;;
+    # Backout our files on failure.
+    if has_failed; then
+      [ -e "script.example.sh" ] && rm "script.example.sh"
+      [ -e "script.example.yml" ] && rm "script.example.yml"
+      [ -e "script.example.json" ] && rm "script.example.json"
+      [ -e $basename ] && rm $basename
+      [ -e $config_file ] && rm $config_file
+      [ -e cloudy ] && rm -r cloudy
+    fi
+  fi
+  has_failed && exit_with_failure "Failed to install $basename"
+  write_log_notice "Installed new script at $WDIR/$basename"
+  exit_with_success_elapsed "New script $basename created."
+  ;;
 
 esac
 
