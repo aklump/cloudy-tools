b4965faa9b5ba227c19577e57d2e5e0f96923d2a
Aaron Klump <sourcecode@intheloftstudios.com>
2022-07-19 14:27:40 -0700

    Fix build step.

--- .web_package/hooks/build/00_composer_update.sh
+++ .web_package/hooks/build/00_composer_update.sh
@@ -4,7 +4,7 @@
 #
 
 composer=$(type composer >/dev/null 2>&1 && which composer)
-cd "$7/framework/cloudy/php" && $composer update --optimize-autoloader
+cd "$7/framework/cloudy" && $composer update --optimize-autoloader
 
-git add "$7/framework/cloudy/php/vendor"
-git add "$7/framework/cloudy/php/composer.lock"
+git add "$7/framework/cloudy/vendor"
+git add "$7/framework/cloudy/composer.lock"
