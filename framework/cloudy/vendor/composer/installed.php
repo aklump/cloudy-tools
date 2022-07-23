0e5dfd8006dc07123cbd4c9047eef7b99ba6ec5f
Aaron Klump <sourcecode@intheloftstudios.com>
2022-07-22 18:33:27 -0700

    New release 1.4.4.

--- framework/cloudy/vendor/composer/installed.php
+++ framework/cloudy/vendor/composer/installed.php
@@ -1,7 +1,7 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.4.2',
-        'version' => '1.4.2.0',
+        'pretty_version' => '1.4.3',
+        'version' => '1.4.3.0',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,8 +11,8 @@
     ),
     'versions' => array(
         'aklump/cloudy' => array(
-            'pretty_version' => '1.4.2',
-            'version' => '1.4.2.0',
+            'pretty_version' => '1.4.3',
+            'version' => '1.4.3.0',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
