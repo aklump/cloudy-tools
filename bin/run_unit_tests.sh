25dead618e7cad8a9e144f46aa75e794d92ffe15
Aaron Klump <sourcecode@intheloftstudios.com>
2024-06-29 14:09:38 -0700

    Update root unit test runner.

--- bin/run_unit_tests.sh
+++ bin/run_unit_tests.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 
-source="${BASH_SOURCE[0]}"
-source="${BASH_SOURCE[0]}"
-if [[ ! "$source" ]]; then
-  source="${(%):-%N}"
-fi
-while [ -h "$source" ]; do # resolve $source until the file is no longer a symlink
-  dir="$( cd -P "$( dirname "$source" )" && pwd )"
-  source="$(readlink "$source")"
-  [[ $source != /* ]] && source="$dir/$source" # if $source was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-cd -P "$( dirname "$source" )/../" && ./cloudy_tools.sh tests
+s="${BASH_SOURCE[0]}";[[ "$s" ]] || s="${(%):-%N}";while [ -h "$s" ];do d="$(cd -P "$(dirname "$s")" && pwd)";s="$(readlink "$s")";[[ $s != /* ]] && s="$d/$s";done;__DIR__=$(cd -P "$(dirname "$s")" && pwd)
+
+cd "$__DIR__/../"
+./cloudy_tools.sh tests || exit 1
+./framework/cloudy/bin/run_unit_tests.sh || exit 1
