2459758a412aeada318dd3f70ae932e190e50678
Aaron Klump <sourcecode@intheloftstudios.com>
2024-07-23 16:23:15 -0700

    Skip Cloudy BASH tests when filter option is used
    A conditional statement has been added in the script run_all_tests.sh to not execute Cloudy BASH tests when the --filter option is used. The changes also pass all command line arguments to both unit and integration tests.

--- bin/run_all_tests.sh
+++ bin/run_all_tests.sh
@@ -3,6 +3,11 @@
 s="${BASH_SOURCE[0]}";[[ "$s" ]] || s="${(%):-%N}";while [ -h "$s" ];do d="$(cd -P "$(dirname "$s")" && pwd)";s="$(readlink "$s")";[[ $s != /* ]] && s="$d/$s";done;__DIR__=$(cd -P "$(dirname "$s")" && pwd)
 
 cd "$__DIR__/../"
-./cloudy_tools.sh tests || exit 1
-./framework/cloudy/bin/run_unit_tests.sh || exit 1
-./framework/cloudy/bin/run_integration_tests.sh || exit 1
+
+# Do not run the Cloudy BASH tests if --filter is used.
+if [[ " $* " != *" --filter "* ]]; then
+  ./cloudy_tools.sh tests || exit 1
+fi
+
+./framework/cloudy/bin/run_unit_tests.sh $@ || exit 1
+./framework/cloudy/bin/run_integration_tests.sh $@ || exit 1
