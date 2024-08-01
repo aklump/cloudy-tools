beb5d86aeec7ab26697a8aace5324817d1dbd4e3
Aaron Klump <sourcecode@intheloftstudios.com>
2024-08-01 15:57:44 -0700

    Update version and refactor bootstrap initialization
    Refactor the bootstrap script to use `declare -rx` for variable declarations and adjust how directory paths are resolved and variable names.

--- framework/cloudy/cloudy.sh
+++ framework/cloudy/cloudy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Cloudy version 1.8.1
+# Cloudy version 1.7.12
 
 # Begin Cloudy Core Bootstrap
-export SCRIPT="$s";export ROOT="$r";export WDIR="$PWD";s="${BASH_SOURCE[0]}";while [ -h "$s" ];do dir="$(cd -P "$(dirname "$s")" && pwd)";s="$(readlink "$s")";[[ $s != /* ]] && s="$dir/$s";done;export CLOUDY_ROOT="$(cd -P "$(dirname "$s")" && pwd)";source "$CLOUDY_ROOT/inc/cloudy.api.sh" || exit_with_failure "Missing cloudy/inc/cloudy.api.sh";export CLOUDY_NAME="$(path_filename $SCRIPT)";source "$CLOUDY_ROOT/inc/cloudy.functions.sh" || exit_with_failure "Missing cloudy/inc/cloudy.functions.sh";source "$CLOUDY_ROOT/inc/cloudy.core.sh" || exit_with_failure "Missing cloudy/inc/cloudy.core.sh"
+CLOUDY_PACKAGE_CONTROLLER="$s";declare -rx ROOT="$r";declare -rx CLOUDY_START_DIR="$PWD";s="${BASH_SOURCE[0]}";while [ -h "$s" ];do dir="$(cd -P "$(dirname "$s")" && pwd)";s="$(readlink "$s")";[[ $s != /* ]] && s="$dir/$s";done;[[ "$CLOUDY_CORE_DIR" ]] || CLOUDY_CORE_DIR="$(dirname "$s")";declare -rx CLOUDY_CORE_DIR="$(cd "$CLOUDY_CORE_DIR" && pwd)";source "$CLOUDY_CORE_DIR/inc/cloudy.api.sh" || exit_with_failure "Missing cloudy/inc/cloudy.api.sh";source "$CLOUDY_CORE_DIR/inc/cloudy.functions.sh" || exit_with_failure "Missing cloudy/inc/cloudy.functions.sh";source "$CLOUDY_CORE_DIR/inc/cloudy.core.sh" || exit_with_failure "Missing cloudy/inc/cloudy.core.sh";
